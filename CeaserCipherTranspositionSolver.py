7babf8a847cc531833b2a50eaae869d9050d4b40
Viper-one-one <taylor.j.nastally@gmail.com>
2023-10-16 01:45:28 -0700

    transposition now works, needed a str(key) and a shift in methedology

--- CeaserCipherTranspositionSolver.py
+++ CeaserCipherTranspositionSolver.py
@@ -5,7 +5,9 @@ transposition_result = ""
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 cipher_text = "KUHPVIBQKVOSHWHXBPOFUXHRPVLLDDWVOSKWPREDDVVIDWQRBHBGLLBBPKQUNRVOHQEIRLWOKKRDD"
 
-def decrypt_ceasar(cipher_text: str, shift_amount: int):
+#https://brilliant.org/wiki/caesar-cipher/
+#source for caesar cipher
+def decrypt_caesar(cipher_text: str, shift_amount: int):
     translation = ""
     for symbol in cipher_text:
         if symbol in alphabet:
@@ -16,43 +18,55 @@ def decrypt_ceasar(cipher_text: str, shift_amount: int):
             translation = translation + alphabet[num]
         else:
             translation = translation + symbol
-#    print(f"caeser decode result: {plain_text}")
     return translation
 
-#https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_decryption_of_transposition_cipher.htm
+#https://www.geeksforgeeks.org/columnar-transposition-cipher/
 #source for this code that doesnt even work
 def decrypt_transposition(cipher: str, key: int): 
-    numOfColumns = math.ceil(len(cipher) / key)
-    numOfRows = key
-    numOfShadedBoxes = (numOfColumns * numOfRows) - len(cipher)
-    plaintext = [''] * numOfColumns
-    col = 0
-    row = 0
-   
-    for symbol in cipher:
-        plaintext[col] += symbol
-        col += 1
-        if (col == numOfColumns) or (col == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
-            col = 0
-            row += 1
-    return ''.join(plaintext)
+    key = str(key)
+    decrypted_message = ""
+    rows = math.ceil(len(cipher) / len(key))
+    diff = rows * len(key) - len(cipher)
+    empty_matrix = [""] * len(key)
+    key = key.upper()
+    dictKey = {}
+    for x in range(len(key)):
+        dictKey[x] = key[x]
+    sorted_key = sorted(dictKey.items(), key=lambda pair: pair[1], reverse=False)
+    row_pointer = 0
+    char_pointer = 0
+    for i in range(len(sorted_key)):
+        col = sorted_key[i][0]
+        if col >= len(key) - diff:
+            rowsMax = rows - 1
+        else:
+            rowsMax = rows
+        for z in range(rowsMax):
+            if char_pointer == len(cipher):
+                break
+            empty_matrix[col] += cipher[char_pointer]
+            char_pointer += 1
+    decrypted_message = ''
+    for row in range(rows):
+        for i in range(len(key)):
+            if row < len(empty_matrix[i]):
+                decrypted_message += empty_matrix[i][row]
+    return decrypted_message
 
-#for j in range(1, 999999999):
-#    transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", j)
-#print(list(permutations(list("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA"))))
 
-for j in range(1, 9999999, 1):
-        transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", j)#solution from ceasar cipher
-        print(transposition_result)
+#for j in range(1, 9999999, 1):
+#        transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", j)#solution from ceasar cipher
+#        print(transposition_result)
+        
+#transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", 7315426)#solution from transposition
+#print(transposition_result)
 
+#brute force method I used to find everything below
 index = 0
 while index <= 26:
-    ceasar_result = decrypt_ceasar(cipher_text, index)
+    ceasar_result = decrypt_caesar(cipher_text, index) #"HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA"
     print(ceasar_result)
     for j in range(1, 9999999, 1):
-        transposition_result = decrypt_transposition(ceasar_result, j)
+        transposition_result = decrypt_transposition(ceasar_result, j) #7315426
         print(transposition_result)
-    #transposition_result = decrypt_transposition(10, cipher_text)
-    #ceasar_result = decrypt_ceasar(transposition_result, i)
-    #print(ceasar_result)
     index += 1
