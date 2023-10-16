bb762666beec0126d96bfa3ba8b534c2086ca6d9
Viper-one-one <taylor.j.nastally@gmail.com>
2023-10-16 01:21:44 -0700

    new version, transposition still doesnt work, solution from ceasar now known so total runtime should be decreased substantially

--- CeaserCipherTranspositionSolver.py
+++ CeaserCipherTranspositionSolver.py
@@ -1,42 +1,58 @@
-import itertools
 import math
-from pprint import pprint
-import threading
 
-key_len = 10
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-key = "aaaaaaaaaa"
+ceasar_result = ""
+transposition_result = ""
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+cipher_text = "KUHPVIBQKVOSHWHXBPOFUXHRPVLLDDWVOSKWPREDDVVIDWQRBHBGLLBBPKQUNRVOHQEIRLWOKKRDD"
 
-def decrypt_ceasar_dev(cipher_text, shift_amount):
-    cipher_text = cipher_text.lower()
-    plain_text = ""
-    for character in cipher_text:
-        position = alphabet.index(character)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
+def decrypt_ceasar(cipher_text: str, shift_amount: int):
+    translation = ""
+    for symbol in cipher_text:
+        if symbol in alphabet:
+            num = alphabet.find(symbol)
+            num = num - shift_amount
+            if num < 0:
+                num = num + len(alphabet)
+            translation = translation + alphabet[num]
+        else:
+            translation = translation + symbol
 #    print(f"caeser decode result: {plain_text}")
-    return plain_text
-    
-def decrypt_transposition_geeks(cipher_text):
-    cols = math.ceil(len(cipher)/key_len)
-    rows = key_len-1
-    trans_matrix = [[0 for x in range(cols)] for x in range(rows)]
-    for i in range(rows):
-        for j in range(cols):
-            trans_matrix[i][j] = cipher[i * cols + j]
-    trans_matrix[:] = itertools.permutations(trans_matrix[:])
-    return trans_matrix
+    return translation
 
-cipher = "KUHPVIBQKVOSHWHXBPOFUXHRPVLLDDWVOSKWPREDDVVIDWQRBHBGLLBBPKQUNRVOHQEIRLWOKKRDD"
-i = 0
-trans_ceasar = ""
-trans_trans = ""
-while i <= 26:
-    trans_ceasar = decrypt_ceasar_dev(cipher, i)
-    print(trans_ceasar)
-    i = i + 1
-    trans_trans = decrypt_transposition_geeks(trans_ceasar)
-    t = threading.Thread(target=pprint, args=trans_trans)
-    t.daemon = True
-    t.start()
+#https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_decryption_of_transposition_cipher.htm
+#source for this code that doesnt even work
+def decrypt_transposition(cipher: str, key: int): 
+    numOfColumns = math.ceil(len(cipher) / key)
+    numOfRows = key
+    numOfShadedBoxes = (numOfColumns * numOfRows) - len(cipher)
+    plaintext = [''] * numOfColumns
+    col = 0
+    row = 0
+   
+    for symbol in cipher:
+        plaintext[col] += symbol
+        col += 1
+        if (col == numOfColumns) or (col == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
+            col = 0
+            row += 1
+    return ''.join(plaintext)
 
+#for j in range(1, 999999999):
+#    transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", j)
+#print(list(permutations(list("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA"))))
+
+for j in range(1, 9999999, 1):
+        transposition_result = decrypt_transposition("HREMSFYNHSLPETEUYMLCRUEOMSIIAATSLPHTMOBAASSFATNOYEYDIIYYMHNRKOSLENBFOITLHHOAA", j)#solution from ceasar cipher
+        print(transposition_result)
+
+index = 0
+while index <= 26:
+    ceasar_result = decrypt_ceasar(cipher_text, index)
+    print(ceasar_result)
+    for j in range(1, 9999999, 1):
+        transposition_result = decrypt_transposition(ceasar_result, j)
+        print(transposition_result)
+    #transposition_result = decrypt_transposition(10, cipher_text)
+    #ceasar_result = decrypt_ceasar(transposition_result, i)
+    #print(ceasar_result)
+    index += 1
